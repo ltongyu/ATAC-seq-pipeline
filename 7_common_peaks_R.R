9eabffb00004376f2589d66752e9dc3876c72c7b
ltongyu <ltongyu@umich.edu>
2020-02-04 13:42:27 -0500

    Update 7_common_peaks_R.R

--- 7_common_peaks_R.R
+++ 7_common_peaks_R.R
@@ -4,10 +4,10 @@ summary(x)
 
 ctrl_common <- ctrl [ which (ctrl$num > 1), 1:3]
 
-LKO <- read.table ('/path/to/nonblacklist/common_cre.v.2', header = T)
+LKO <- read.table ('/path/to/nonblacklist/common_cre', header = T)
 y <- as.factor(LKO$num)
 summary(y)
 
 LKO_common <- LKO [ which (LKO$num > 1), 1:3]
-write.table(ctrl_common, '/path/to/nonblacklist/ctrl_common.v.3', sep="\t", col.names=F, row.names=F, quote = F)
-write.table(LKO_common, '/path/to/nonblacklist/LKO_common.v.3', sep="\t", col.names=F, row.names=F, quote = F)
+write.table(ctrl_common, '/path/to/nonblacklist/ctrl_common', sep="\t", col.names=F, row.names=F, quote = F)
+write.table(LKO_common, '/path/to/nonblacklist/LKO_common', sep="\t", col.names=F, row.names=F, quote = F)

fbfaf8f321bef1d33da0a3076f88a88565844d6c
ltongyu <ltongyu@umich.edu>
2020-02-04 13:51:18 -0500

    Update 9_count_reads_in_each_peak.sh

--- 9_count_reads_in_each_peak.sh
+++ 9_count_reads_in_each_peak.sh
@@ -3,3 +3,7 @@ cd $1
 coverageBed -counts -sorted -a /path/to/nonblacklist/master_peaks_beige.sorted.bed -b $1.hqaa.bam > /path/to/Rinput/$1.counts.bed
 
 cd ..
+
+
+cd /path/to/Rinput/
+for f in *.counts.bed; do echo $f;awk '{print $1":"$2"-"$3"\t"$4}' $f > $f.Rinput;done
